3651e8bf7685ef6df98939fbacf339da624322fa
root <root@ip-172-31-0-83.ec2.internal>
2018-02-17 12:36:59 +0000

    third

--- html/index.php
+++ html/index.php
@@ -4,7 +4,7 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?><!DOCTYPE html>
 <html>
   <head>
-    <title>Welcome to BHASKAR THE bOSS Technical Essentials v4.1</title>
+    <title>Welcome to Ritika The Heroin Technical Essentials v4.1</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
   </head>
